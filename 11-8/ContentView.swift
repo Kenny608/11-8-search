93779e01446f9d686271a3b2ebe1eda635bc2a75
Kenny <kennypluchau@gmail.com>
2023-11-20 14:29:25 -0800

    init

---
+++ 11-8/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  11-8
+//
+//  Created by Kenny Luchau on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 11-8/_1_8App.swift
@@ -0,0 +1,17 @@
+//
+//  _1_8App.swift
+//  11-8
+//
+//  Created by Kenny Luchau on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct _1_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
